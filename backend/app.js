e41550c6623d3e664a10b10ccab6c93881913abf
Henok <zakbinayew@gmail.com>
2025-04-12 22:29:32 +0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 //db conn
 const dbConn = require("./db/dbConfig");
 
