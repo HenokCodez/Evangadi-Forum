6f8b34af96686ea106bc250226fc32c79adfdb07
Henok <zakbinayew@gmail.com>
2025-04-14 22:46:54 +0300

    deploy

--- front-end/src/API/axiosConfig.js
+++ front-end/src/API/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://evangadi-forum-2-1zrc.onrender.com/api", // Use the deployed backend URL
   headers: {
     "Content-Type": "application/json",
   },
